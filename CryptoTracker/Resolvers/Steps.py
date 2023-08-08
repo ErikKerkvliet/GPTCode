d3867b18900996ba5a39b4e84bc49e9d2d4920a7
Erik Kerkvliet <e.kerkvliet@troublefree.nl>
2023-08-08 17:14:25 +0200

    quick

--- CryptoTracker/Resolvers/Steps.py
+++ CryptoTracker/Resolvers/Steps.py
@@ -10,17 +10,12 @@ class Steps:
         if crypto.rate is not None:
             code = crypto.code if len(crypto.code) == 4 else f'{crypto.code} '
             difference = crypto.rate / crypto.buy_rate
-            percentage = round((difference - 1), 5)
+            percentage = round((difference - 1), 5) * 100
             amount = round(difference * globalvar.BUY_AMOUNT, 5)
-            sell = '✓✓✓✓' if amount > globalvar.BUY_AMOUNT * 1.01 else '⤫'
-            if percentage < 0:
-                percentage_colored = f'\033[91m{percentage}\033[0m'
-            elif percentage > 0:
-                percentage_colored = f'\033[92m{percentage}\033[0m'
-            else:
-                percentage_colored = percentage
+            sell = '\033[92m✓✓✓✓\033[0m' if amount > globalvar.BUY_AMOUNT * 1.01 else '\033[91m⤫\033[0m'
+            percentage = self.colorize(percentage, 91, 92)
 
-            print(f'Coin: {code}, Difference: {percentage_colored}%, Amount €: {amount}, Drops: {crypto.drops}, Sell: {sell}')
+            print(f'Coin: {code}, Difference: {percentage}%, Amount €: {amount}, Drops: {crypto.drops}, Sell: {sell}')
 
         if crypto.balance == 0 or crypto.last_rate is None or crypto.rate == crypto.last_rate:
             return False
@@ -69,3 +64,13 @@ class Steps:
 
             if crypto.drops < 2:
                 crypto.drops += 1
+
+    @staticmethod
+    def colorize(var1, color1, color2):
+        var = f'{float(var1):5f}'
+        if var1 < 0:
+            return f'\033[{color1}m{var}\033[0m'
+        elif var1 > 0:
+            return f'\033[{color2}m{var}\033[0m'
+        else:
+            return var1
